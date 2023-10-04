ed3b2e7502bfb4c25a2d48f0acc1e2c6858986c9
rmavuluri <rummy.mavuluri@outlook.com>
2023-10-03 22:18:53 -0400

    middleware introduced

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,7 +1,13 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key/api-key.guard';
 import { ConfigModule } from '@nestjs/config';
+import { LoggingMiddleware } from './middleware/logging/logging.middleware';
 
 @Module({
   imports: [ConfigModule],
@@ -12,4 +18,12 @@ import { ConfigModule } from '@nestjs/config';
     },
   ],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+    //consumer.apply(LoggingMiddleware).exclude('courses').forRoutes('*');
+    // consumer
+    //   .apply(LoggingMiddleware)
+    //   .forRoutes({ path: 'courses', method: RequestMethod.GET });
+  }
+}
